d7aea9d93e8a5e11d58d8a2e1f04df65ec81f84f
AlejandroReyes20 <84936999+AlejandroReyes20@users.noreply.github.com>
2021-05-27 16:20:29 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,16 @@
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBZw4Z7Z-VrMnTpP9t_mbDSTXSAT5Sb0eg",
+    authDomain: "fbwebcesar21.firebaseapp.com",
+    projectId: "fbwebcesar21",
+    storageBucket: "fbwebcesar21.appspot.com",
+    messagingSenderId: "311975862469",
+    appId: "1:311975862469:web:4ed50a5badcb8af7be90ac",
+    measurementId: "G-BTDYFVVY7Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
