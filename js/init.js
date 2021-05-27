18731a4ec4d9338b8cf545a01fc09ad9b2cc6825
AlejandroReyes20 <alejandro.reyes2000.cacr@gmail.com>
2021-05-27 17:38:48 -0500

    cambio

--- js/init.js
+++ js/init.js
@@ -1,14 +1,13 @@
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBZw4Z7Z-VrMnTpP9t_mbDSTXSAT5Sb0eg",
+/*Initializa Firebase con la configuracion del proecto. Revisa la 
+* configuracion en tu servidor Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+	apiKey: "AIzaSyBZw4Z7Z-VrMnTpP9t_mbDSTXSAT5Sb0eg",
   authDomain: "fbwebcesar21.firebaseapp.com",
   projectId: "fbwebcesar21",
   storageBucket: "fbwebcesar21.appspot.com",
   messagingSenderId: "311975862469",
   appId: "1:311975862469:web:4ed50a5badcb8af7be90ac",
   measurementId: "G-BTDYFVVY7Z"
-};
-// Initialize Firebase
-
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+});
